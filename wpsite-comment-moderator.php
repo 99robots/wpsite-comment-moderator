ba88525b19dcafd5447be6994c5774f3590cbeaf
umarnaeem1 <umarnaeem01@gmail.com>
2020-12-28 23:28:08 +0500

    Compatible with WordPress 5.6

--- wpsite-comment-moderator.php
+++ wpsite-comment-moderator.php
@@ -3,7 +3,7 @@
  * Plugin Name:	Comment Moderator
  * Plugin URI:	https://draftpress.com/products/
  * Description:	Add a new user role, Comment Moderator, that allows a new user to only manage comments.
- * Version: 1.3.5
+ * Version: 1.3.6
  * Author: 99 Robots
  * Author URI:	https://draftpress.com
  * License: GPL2
@@ -31,7 +31,7 @@ if ( ! defined( 'WPSITE_COMMENT_MODERATOR_PLUGIN_URL' ) ) {
 
 // Plugin verison
 if ( ! defined( 'WPSITE_COMMENT_MODERATOR_VERSION_NUM' ) ) {
-	define( 'WPSITE_COMMENT_MODERATOR_VERSION_NUM', '1.3.5' );
+	define( 'WPSITE_COMMENT_MODERATOR_VERSION_NUM', '1.3.6' );
 }
 
 /**
@@ -49,7 +49,7 @@ class WPsiteCommentModerator {
 	/**
 	 * The Constructor
 	 *
-	 * @since 1.3.2
+	 * @since 1.3.6
 	 */
 	public function __construct() {
 
