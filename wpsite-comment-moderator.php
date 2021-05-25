b7cde09046cea49f9a12f976f2acd74c1a03274b
abhishek <abhij89@gmail.com>
2021-05-25 19:04:16 +0530

    = 1.3.7 = 2021-14-05 * Compatible with WordPress 5.7.2

--- wpsite-comment-moderator.php
+++ wpsite-comment-moderator.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * Plugin Name:	Comment Moderator
- * Plugin URI:	https://draftpress.com/products/
- * Description:	Add a new user role, Comment Moderator, that allows a new user to only manage comments.
- * Version: 1.3.6
+ * Plugin Name:    Comment Moderator
+ * Plugin URI:    https://draftpress.com/products/
+ * Description:    Add a new user role, Comment Moderator, that allows a new user to only manage comments.
+ * Version: 1.3.7
  * Author: 99 Robots
- * Author URI:	https://draftpress.com
+ * Author URI:    https://draftpress.com
  * License: GPL2
- * Text Domain:	wpsite-comment-moderator
+ * Text Domain:    wpsite-comment-moderator
  */
 
 /**
@@ -15,109 +15,114 @@
  */
 
 // Plugin Name
-if ( ! defined( 'WPSITE_COMMENT_MODERATOR_PLUGIN_NAME' ) ) {
-	define( 'WPSITE_COMMENT_MODERATOR_PLUGIN_NAME', trim( dirname( plugin_basename( __FILE__ ) ), '/' ) );
+if (!defined('WPSITE_COMMENT_MODERATOR_PLUGIN_NAME')) {
+    define('WPSITE_COMMENT_MODERATOR_PLUGIN_NAME', trim(dirname(plugin_basename(__FILE__)), '/'));
 }
 
 // Plugin directory
-if ( ! defined( 'WPSITE_COMMENT_MODERATOR_PLUGIN_DIR' ) ) {
-	define( 'WPSITE_COMMENT_MODERATOR_PLUGIN_DIR', WP_PLUGIN_DIR . '/' . WPSITE_COMMENT_MODERATOR_PLUGIN_NAME );
+if (!defined('WPSITE_COMMENT_MODERATOR_PLUGIN_DIR')) {
+    define('WPSITE_COMMENT_MODERATOR_PLUGIN_DIR', WP_PLUGIN_DIR . '/' . WPSITE_COMMENT_MODERATOR_PLUGIN_NAME);
 }
 
 // Plugin url
-if ( ! defined( 'WPSITE_COMMENT_MODERATOR_PLUGIN_URL' ) ) {
-	define( 'WPSITE_COMMENT_MODERATOR_PLUGIN_URL', WP_PLUGIN_URL . '/' . WPSITE_COMMENT_MODERATOR_PLUGIN_NAME );
+if (!defined('WPSITE_COMMENT_MODERATOR_PLUGIN_URL')) {
+    define('WPSITE_COMMENT_MODERATOR_PLUGIN_URL', WP_PLUGIN_URL . '/' . WPSITE_COMMENT_MODERATOR_PLUGIN_NAME);
 }
 
 // Plugin verison
-if ( ! defined( 'WPSITE_COMMENT_MODERATOR_VERSION_NUM' ) ) {
-	define( 'WPSITE_COMMENT_MODERATOR_VERSION_NUM', '1.3.6' );
+if (!defined('WPSITE_COMMENT_MODERATOR_VERSION_NUM')) {
+    define('WPSITE_COMMENT_MODERATOR_VERSION_NUM', '1.3.7');
 }
 
 /**
  * Activatation / Deactivation
  */
-register_activation_hook( __FILE__, array( 'WPsiteCommentModerator', 'register_activation' ) );
+register_activation_hook(__FILE__, array('WPsiteCommentModerator', 'register_activation'));
 
 /**
  * WPsiteCommentModerator
  *
  * @since 1.0.0
  */
-class WPsiteCommentModerator {
+class WPsiteCommentModerator
+{
 
-	/**
-	 * The Constructor
-	 *
-	 * @since 1.3.6
-	 */
-	public function __construct() {
+    /**
+     * The Constructor
+     *
+     * @since 1.3.7
+     */
+    public function __construct()
+    {
 
-		add_action( 'init', array( $this, 'load_textdomain' ) );
-		add_action( 'admin_menu', array( $this, 'remove_menu' ) );
-	}
+        add_action('init', array($this, 'load_textdomain'));
+        add_action('admin_menu', array($this, 'remove_menu'));
+    }
 
-	/**
-	 * Load the text domain
-	 *
-	 * @since 1.0.0
-	 */
-	public function load_textdomain() {
-		load_plugin_textdomain( 'wpsite-comment-moderator', false, WPSITE_COMMENT_MODERATOR_PLUGIN_DIR . '/languages' );
-	}
+    /**
+     * Load the text domain
+     *
+     * @since 1.0.0
+     */
+    public function load_textdomain()
+    {
+        load_plugin_textdomain('wpsite-comment-moderator', false, WPSITE_COMMENT_MODERATOR_PLUGIN_DIR . '/languages');
+    }
 
-	/**
-	 * Remove menu according to role
-	 */
-	public function remove_menu() {
+    /**
+     * Remove menu according to role
+     */
+    public function remove_menu()
+    {
 
-		$user = wp_get_current_user();
+        $user = wp_get_current_user();
 
-		if ( ! empty( $user ) && in_array( 'comment_moderator', (array) $user->roles ) ) {
+        if (!empty($user) && in_array('comment_moderator', (array) $user->roles)) {
 
-			remove_menu_page( 'edit.php' );
-			remove_menu_page( 'tools.php' );
+            remove_menu_page('edit.php');
+            remove_menu_page('tools.php');
 
-			$post_types = get_post_types( '', 'names' );
+            $post_types = get_post_types('', 'names');
 
-			foreach ( $post_types as $post_type ) {
-				remove_menu_page( 'edit.php?post_type=' . $post_type );
-			}
-		}
-	}
+            foreach ($post_types as $post_type) {
+                remove_menu_page('edit.php?post_type=' . $post_type);
+            }
+        }
+    }
 
-	/**
-	 * Hooks to 'register_activation_hook'
-	 *
-	 * @since 1.0.0
-	 */
-	static function register_activation() {
+    /**
+     * Hooks to 'register_activation_hook'
+     *
+     * @since 1.0.0
+     */
+    public static function register_activation()
+    {
 
-		// Check if multisite, if so then save as site option
-		if ( is_multisite() ) {
-			add_site_option( 'wpsite_comment_moderator_version', WPSITE_COMMENT_MODERATOR_VERSION_NUM );
-		} else {
-			add_option( 'wpsite_comment_moderator_version', WPSITE_COMMENT_MODERATOR_VERSION_NUM );
-		}
+        // Check if multisite, if so then save as site option
+        if (is_multisite()) {
+            add_site_option('wpsite_comment_moderator_version', WPSITE_COMMENT_MODERATOR_VERSION_NUM);
+        } else {
+            add_option('wpsite_comment_moderator_version', WPSITE_COMMENT_MODERATOR_VERSION_NUM);
+        }
 
-		remove_role( 'comment_moderator' );
+        remove_role('comment_moderator');
 
-		add_role(
-			'comment_moderator',
-			esc_html__( 'Comment Moderator', 'wpsite-comment-moderator' ),
-			array(
-				'read'					=> true,
-				'moderate_comments' 	=> true,
-				'edit_comment'			=> true,
-				'edit_others_posts'		=> true,
-				'edit_published_posts'	=> true,
-				'edit_posts'			=> true,
-				'edit_others_pages'		=> true,
-				'edit_published_pages'	=> true,
-				'edit_pages'			=> true,
-			)
-		);
-	}
+        add_role(
+            'comment_moderator',
+            esc_html__('Comment Moderator', 'wpsite-comment-moderator'),
+            array(
+                'read' => true,
+                'moderate_comments' => true,
+                'edit_comment' => true,
+                'edit_others_posts' => true,
+                'edit_published_posts' => true,
+                'edit_posts' => true,
+                'edit_others_pages' => true,
+                'edit_published_pages' => true,
+                'edit_pages' => true,
+            )
+        );
+    }
 }
 
 /**
